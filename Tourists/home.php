bfb0e79b7fddd4e0132f4e583b7e4e85176fa73a
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-12-17 14:06:50 +0530

    update user registration process

---
+++ Tourists/home.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_email'])) {
+    header("Location: signup.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - TourHaven</title>
+</head>
+<body>
+    <h1>Welcome, <?php echo $_SESSION['user_name']; ?>!</h1>
+    <p>You have successfully signed up with your Google Account.</p>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
--- Tourists/login.php
+++ Tourists/login.php
@@ -1,19 +1,32 @@
 <?php
     session_start();
-    require '../conn.php';
+    require '../vendor/autoload.php'; // Include the Composer autoload file
 
+    // Google API Client
+    $client = new Google_Client();
+    $client->setClientId('274839241301-b7o0dvlceptppe87iv4qk79h2ml7spee.apps.googleusercontent.com');
+    $client->setClientSecret('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');
+    $client->setRedirectUri('http://localhost:3000/Tourists/google-signup-callback.php'); // Must match exactly in Google Console
+    $client->addScope('email');
+    $client->addScope('profile');
+
+    // Google Login URL
+    $google_login_url = $client->createAuthUrl();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TourHaven</title>
-    <link rel="shortcut icon" href="../res/logo.jpg">
+    <title>TourHaven - Login</title>
 </head>
 <body>
-    
+    <h2>Welcome to TourHaven</h2>
+    <a href="<?php echo $google_login_url; ?>">
+        <button style="padding: 10px 20px; background-color: #4285F4; color: white; border: none; cursor: pointer;">
+            Login with Google
+        </button>
+    </a>
 </body>
 </html>
