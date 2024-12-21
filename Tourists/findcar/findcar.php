7c9175c2e2a37c83dd454f891b79d9cac2b87538
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2024-12-21 23:57:07 +0530

    update tourist's side

--- Tourists/findcar/findcar.php
+++ Tourists/findcar/findcar.php
@@ -1,8 +1,12 @@
 <?php
-session_start();
+session_start(); // Start session at the beginning
+if (!isset($_SESSION['email'])) {
+    header('location: ../login.php'); // Redirect to login if session is not set
+    exit;
+}
+
 require('../nav.php');
 require('../../conn.php');
-
 ?>
 
 <!DOCTYPE html>
@@ -12,23 +16,24 @@ require('../../conn.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../css/find.css">
+    <title>Find Car</title>
 </head>
-
 <body>
     <div class="top">
-            <div class="topic">
-                <h1 class="first">Expert Car Repair Services,</h1>
-                <h1>Wherever You are?</h1>
-                <h1>We're Here to Help</h1>
+        <div class="topic">
+            <h1 class="first">Do you need a Travel Companion</h1>
+            <p>
+                Imagine discovering new destinations with a trusted companion by your side. Whether you need someone to share the journey or
+                help during a challenging moment, we’re here to make your travel plans stress-free. Let’s connect you with like-minded
+                travelers and reliable assistance when you need it most.
+            </p>
+            <button class="button" onclick="window.location.href='../post/post.php'">Find</button>
+            <button class="button" onclick="window.location.href='breakdown_details.php'">Breakdowns</button>
 
-
-            <a href="../post/post.php" class="button">Find</a>
-            <a href="breakdown_details.php" class="button">Breakdowns</a>
-
-            </div>
-            <div class="side-img">
-            <img src="../../img/mechnic.png" alt="mechanic image">
-            </div>
         </div>
+        <div class="side-img">
+            <img src="../../res/about2.jpg" alt="mechanic image">
+        </div>
+    </div>
 </body>
 </html>
